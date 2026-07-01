bd8ae52c2bf8551113480dc9b720b4e142240d5d
VishaliSubramanium <43952038+VishaliSubramanium@users.noreply.github.com>
2026-07-01 19:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
-  // your code here
+  s = s.trim();
+
+  let index = s.indexOf(" ");
+
+  if (index === -1) {
+    return s;
+  }
+
+  return s.substring(0, index);
 }
 
 // Do not change the code below
